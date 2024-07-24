9cd637c2148f41e9018614673a012168779e459a
penguin2437coder <penguin2437coder@penguincoding.org>
2024-07-24 10:52:16 -0400

    Add files via upload
    camp 1&2 initial commit.

---
+++ camp 2.py
@@ -0,0 +1,50 @@
+
+
+#age = int(input(" what is your age?:
+
+                
+#num=int(input(" give me a number:"))
+#num2=int(input(" give me another number:"))
+#sum1 = num + num2
+#print ( " you will be " + str( age + sum1 ) + " in " + str(sum1))
+# give me a
+
+
+
+                
+
+
+
+#temp=int(input( " give me the temperture or else: "))
+#weather=input(" weather conditions ( clear,snow,storm, and psycho):")
+#print ( " good for you ur probably gonna die!!!!!!! have fun.")
+#words=input ( " give me your last words:")
+#answer=input (" would your family pay for those words? ( yes,no):")
+#if words=="yes":
+ #  print ( " exellent what is your adress?")
+#answer3=input (" how much ?:") 
+
+# pick a color number`
+
+
+import random
+
+number= ( random.randint (1, 8) ) 
+question= input ( " what is your question for this dumpy crystal ball:")
+if number==1 :
+    print(" you will get dumped.")
+elif  number==2 :
+    print (" you will die of food poisoning")
+elif number==3:
+    print ( " you will get tortured by a unicorn")
+elif number==4:
+    print ( " you will go heaven ")
+elif number==5:
+    print (" you will be drastic for entirnity")
+elif number==6:
+    print ( " you will be enslaved by kittens ")
+elif number==7:
+    print ( " you will sneeze your head off ")
+elif number==8:
+    print ( " you will eat a live donky ")
+
---
+++ chaos theory.py
@@ -0,0 +1,29 @@
+import os 
+'''
+print ( " chaos theory " )
+name= input ( "what is your name")
+print ( " nice to meet you, " + name+"."+" i love dinosaurs to " )
+print ( " cool! what is your favorite kind?")
+print ( " hmmm, cryolophosaurus ")
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+animal=input (" give me a animal ( plural):")
+name=input ( " give me a name:")
+food =input (" give me a food :")
+movie=input (" give me a  animalmovie: ")
+videogame = input ( " give me a video game:")
+adjective=input (" give me a adjective :")
+bodypart=input ( " give me a body part :")
+story = "i saw a group " + animal + " attack " + name + " by stuffing his mouth with moldy " +food + " while forcing him to watch " + movie+ " and playing " + videogame+ " on his"  + bodypart+ " his siuation is " + adjective
+print(story)
